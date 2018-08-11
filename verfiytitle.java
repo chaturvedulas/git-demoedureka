049d5aed3b46e956952c9ff9146b4ad2962f81a7
Sudheer Chaturvedula <40879341+chaturvedulas@users.noreply.github.com>
2018-08-10 19:34:08 -0500

    Update verfiytitle.java

--- verfiytitle.java
+++ verfiytitle.java
@@ -4,6 +4,7 @@ public class verfiytitle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// this code uses chromedriver to maximize the window and get the tile of the window
 		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
        ChromeDriver driver = new ChromeDriver();
        driver.manage().window().maximize();
