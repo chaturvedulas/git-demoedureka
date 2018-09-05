e77b57fb164fd32e5e189eeef228f59b6185cc5d
Sudheer Chaturvedula <40879341+chaturvedulas@users.noreply.github.com>
2018-09-05 08:26:21 -0500

    Update verifyurl.java

--- verifyurl.java
+++ verifyurl.java
@@ -1,7 +1,5 @@
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 public class verifyurl {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.setProperty("webdriver.gecko.driver","geckodriver.exe");
@@ -10,5 +8,4 @@ public class verifyurl {
          String x =  driver.getCurrentUrl();
          System.out.println(x);
 	}
-
 }
